667fd0643526c89fdde415e2b5b424e1fbbc0741
Alex Stuckey <alex@stuckey.org.uk>
2013-01-26 17:59:00 +0000

    Some basic server side support for the previous commit

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 // Defining configuration
 app.configure(function(){
-       app.set('port', 3000);
-       app.use(express.static(__dirname + '/public'));
+	app.set('port', 3000);
+	app.use(express.static(__dirname + '/public'));
 })
 
 app.get('/', function(req, res){
-       res.send('Home');
+	res.sendfile(__dirname + '/public/index.html');
 })
 
-app.listen(app.get('port'));
+io.sockets.on('connection', function(socket){
+	console.log('Client connected');
+	socket.on('up', function(data){
+		console.log('Message uploaded: ', data);
+		io.sockets.emit('down', data);
+	});
+});
+
+server.listen(app.get('port'));
 console.log('Listening on port ', app.get('port'));
