bc415a285c6b7b8e65bd8c4a1e2bd564d17d2bbb
Alex Stuckey <alex@stuckey.org.uk>
2013-02-07 16:56:49 +0000

    Moving the JS from index.html into logic.js

---
+++ public/js/logic.js
@@ -0,0 +1,47 @@
+var socket = io.connect(window.location.origin);
+
+socket.on('down', function (data) {
+	var msg = JSON.parse(data);
+	if (msg.message != ""){
+		var userName = msg.user;
+		if (msg.user == ""){
+			userName = "Anonymous";
+		}
+		if (msg.channel == $('#channelEntry').val() && msg.subChannel == "") {
+			 	$('#msgLog > tbody:last').append('<tr><td>' + userName + '</td><td>' + msg.message + '</td></tr>');
+		};
+		if (msg.channel == $('#channelEntry').val() && msg.subChannel == $('#subChannelEntry').val() && msg.subChannel != "") {
+			$('#msgLog > tbody:last').append('<tr><td><font color=3399FF>' + userName + '</td><td><font color=3399FF>' + msg.message + '</td></tr>');
+		};
+	};
+  });
+
+socket.on('users', function (data) {
+	console.log('Connected users: ' + data);
+	$('#connectedUsersCell').text(data + " user(s)");
+});
+
+function SendMessage(){
+	var msgToSend = {
+		user: $('#nameEntry').val(),
+		message: $('#textEntry').val(),
+		channel: $('#channelEntry').val(),
+		subChannel: $('#subChannelEntry').val(),
+	};
+	socket.emit('up', JSON.stringify(msgToSend));
+	console.log('Sent message!');
+
+	// Clear textEntry
+	$('#textEntry').val('');
+  };
+
+$(document).ready(function(){
+	// This runs when the page is ready, interface code should be inside here
+	$('#textEntry').keyup(function(event){
+	event = event || window.event;
+	if (event.keyCode == 13) {
+		// on enter key
+		$('#textButton').click();
+		}
+	  });
+  });
